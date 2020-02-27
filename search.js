d6f01689dd70b22fe8d7dc00bbe350d3fe6a925d
Lyle Dickie <lyle.e.dickie@gmail.com>
2020-02-27 06:50:52 -0500

    Initialize Repository

---
+++ search.js
@@ -0,0 +1,29 @@
+// Iterating through Arrays
+
+const arr = [1,2,3]
+
+// for(let i=0; i < arr.length; i++) {
+//   console.log(arr[i])
+// }
+//
+//
+// for(let idx in arr) {
+//   console.log(arr[idx])
+// }
+//
+//
+// for(let el of arr) {
+//   console.log(el)
+// }
+//
+//
+// arr.forEach((el, idx) => {
+//   console.log(el)
+// })
+//
+//
+// arr.forEach((el, idx) => {
+//   console.log(arr[idx])
+// })
+
+// Linear search
