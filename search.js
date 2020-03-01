487a6bcec6e7bae0c6c4aca7b4f79a5ccd5c8cb9
Lyle Dickie <lyle.e.dickie@gmail.com>
2020-03-01 03:11:44 -0500

    Added Binary search

--- search.js
+++ search.js
@@ -1,11 +1,12 @@
 // Search
 
-const arr = [1,2,3]
+const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,21,20]
 
 
-// Linear search
+// Linear search O(n)
 
 const linearSearch = (arr, n) => {
+  console.log('Linear Search')
   for(let i=0; i<arr.length; i++) {
     if(arr[i] == n) {
       console.log('Found in array')
@@ -19,15 +20,32 @@ const linearSearch = (arr, n) => {
 linearSearch(arr, 3)
 
 
-const linearSearch2 = (arr, n) => {
-  for(el in arr){
-    if(el == n){
-      console.log('Found in array')
-      return true
+
+
+// Binary Search O(log n)
+
+const binarySearch = (arr, n) => {
+  console.log('Binary Search')
+  arr.sort(function(a,b) {
+  return (+a) - (+b);
+  })
+  let lowIndex = 0, highIndex = arr.length - 1, midIndex
+  while(lowIndex <= highIndex) {
+    midIndex = Math.floor((lowIndex + highIndex)/2)
+    const consl = console.log('low:', lowIndex, 'mid:', midIndex, 'high:', highIndex)
+    consl
+    if(n == arr[midIndex]) {
+      console.log('Found it:', arr[midIndex], n)
+      return
+    } else if(n > arr[midIndex]) {
+      lowIndex = midIndex + 1
+    } else {
+      highIndex = midIndex - 1
     }
   }
-  console.log('Not found in array')
-  return false
+  console.log('Not found')
+  return -1
 }
 
-linearSearch2(arr, 4)
+
+binarySearch(arr, 2)
